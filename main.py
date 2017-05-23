e9680ad34eaf1b24577dca607925ac094147ef12
root <root@master.puppet.com>
2017-05-23 14:40:51 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+#!/usr/biu/env python2.7
+#coding:utf-8
+from flask import Flask
+from flask import request
+from flask import url_for 
+from flask import render_template 
+app=Flask(__name__)
+@app.route('/',methods=['PUT','POST','GET'])
+def index():
+    return render_template('index.html')
+if __name__=='__main__':
+    app.run(host='0.0.0.0',port=80,debug=True)
