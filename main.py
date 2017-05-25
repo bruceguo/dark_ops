03bbc25312ef67737cca35aede792a05a490f2da
root <root@master.puppet.com>
2017-05-25 13:49:36 +0800

    xx

--- main.py
+++ main.py
@@ -8,5 +8,14 @@ app=Flask(__name__)
 @app.route('/',methods=['GET'])
 def index():
     return render_template('index.html')
+@app.route('/list',methods=['GET'])
+def list():
+    return render_template('lists.html')
+@app.route('/addhost',methods=['GET'])
+def addhost():
+    return render_template('add.html')
+@app.route('/home',methods=['GET'])
+def home():
+    return render_template('home.html')
 if __name__=='__main__':
     app.run(host='0.0.0.0',port=80,debug=True)
