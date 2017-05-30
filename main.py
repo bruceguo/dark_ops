60735db32ca12a305f4260640c7c7acb81566b11
root <root@master.puppet.com>
2017-05-30 22:27:04 +0800

    cc

--- main.py
+++ main.py
@@ -4,25 +4,55 @@ from flask import Flask
 from flask import request
 from flask import render_template
 from flask import jsonify,abort 
-
-app=Flask(__name__)
-
+from model import db,dark_status,app
+import json
 @app.route('/',methods=['GET'])
 def index():
     return render_template('index.html')
 @app.route('/list',methods=['GET'])
 def list():
-    return render_template('lists.html')
+    hostlist=dark_status.query.all()
+    return render_template('lists.html',hostlist=hostlist)
 @app.route('/addhost',methods=['GET'])
 def addhost():
     return render_template('add.html')
 @app.route('/home',methods=['GET'])
 def home():
-    return render_template('home.html')
+    hoststotal=len(dark_status.query.all())
+    normalnum=len(dark_status.query.filter_by(status=1).all())
+    print hoststotal
+    return render_template('home.html',hoststotal=hoststotal,normalnum=normalnum)
 @app.route('/api/collect_dark_status',methods=['POST'])
 def collect_info():
-    if request.values.get("mid",None) request.values.get("update_time",None):
-        #print request.values.get("mid",None):
+    if request.values.get("mid",None) and request.values.get("update_time",None):
+        result=dark_status.query.filter_by(mid= request.values["mid"]).all()
+        if len(result)==0:
+            if request.values["new_config_version"] == request.values["old_config_version"]:
+                status=1
+            else:
+                status=0
+            try:
+                db.session.add(dark_status(mid=request.values["mid"],dark_version=request.values["dark_version"],new_config_version=request.values["new_config_version"],old_config_version=request.values["old_config_version"],status=status,boot_time=request.values["boot_time"],update_time=request.values["update_time"]))
+                db.session.commit()
+            except Exception,e:
+                print e
+                return jsonify({"error":1,"message":str(e)}),10012
+            else:
+                return jsonify({"error":0,"message":"status upload succeed."}),201
+        else:
+            query_result=dark_status.query.filter_by(mid=request.values["mid"]).first()
+            if request.values["new_config_version"] == request.values["old_config_version"]:
+                status=1
+            else:
+                status=0
+            query_result.status=status
+            query_result.dark_version=request.values["dark_version"]
+            query_result.new_config_version=request.values["new_config_version"]
+            query_result.old_config_version=request.values["old_config_version"]
+            query_result.update_time=request.values["update_time"]
+            query_result.boot_time=request.values["boot_time"]
+            db.session.commit()
+            return jsonify({"error":0,"message":"status update succeed."}),201
     else:
         return jsonify({"error":1,"message":"mid and update_time is required"}),10010
 if __name__=='__main__':
--- model.py
+++ model.py
@@ -1,9 +1,8 @@
 from flask import Flask
 from flask_script import Manager
 from flask_sqlalchemy import SQLAlchemy
-
+import json
 app=Flask(__name__)
-app.config['SECRET_KEY'] = 'secret'
 app.config['SQLALCHEMY_DATABASE_URI']='mysql://root:chinatt()^^*@localhost:3306/darkinfo'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=True
 db = SQLAlchemy(app)
@@ -19,6 +18,7 @@ class dark_status(db.Model):
     boot_time = db.Column(db.DateTime, nullable=False)
     update_time = db.Column(db.DateTime, nullable=False)
     def __repr__(self):
-        return '<dark_mid %r>' % self.mid
+#        return '<dark_mid %r>' % self.mid
+        return json.dumps({"mid":self.mid,"dark_version":self.dark_version,"id":self.id,"new_config_version":self.new_config_version,"old_config_version":self.old_config_version,"status":self.old_config_version,"boot_time":self.boot_time,"update_time":self.update_time}) 
 if __name__=="__main__":
     manager.run()
