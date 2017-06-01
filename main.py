d911191de7308ef0e8012ce25713e50e80420116
root <root@master.puppet.com>
2017-06-01 14:02:04 +0800

    xx

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from flask import request
 from flask import render_template
 from flask import jsonify,abort 
 from model import db,dark_status,app
+import time
 import json
 @app.route('/',methods=['GET'])
 def index():
@@ -12,7 +13,19 @@ def index():
 @app.route('/list',methods=['GET'])
 def list():
     hostlist=dark_status.query.all()
-    return render_template('lists.html',hostlist=hostlist)
+    #hostlist=[host.message="dark" if host.new_config_version==host.old_config_version and host.boot_time !="" else host.message="dark" for host in hostlist]
+    hostresult=[]
+    for host in hostlist:
+        if time.time() - time.mktime(time.strptime(str(host.update_time),"%Y-%m-%d %H:%M:%S")) > 300:
+            host.message=u"dark上报异常"
+        else:
+            if host.new_config_version==host.old_config_version and host.boot_time !="" and host.dark_num != 0:
+                host.message=u"dark程序正常"
+            else:
+                host.message=u"dark程序异常"
+        hostresult.append(host)
+
+    return render_template('lists.html',hostlist=hostresult)
 @app.route('/addhost',methods=['GET'])
 def addhost():
     return render_template('add.html')
@@ -24,15 +37,19 @@ def home():
     return render_template('home.html',hoststotal=hoststotal,normalnum=normalnum)
 @app.route('/api/collect_dark_status',methods=['POST'])
 def collect_info():
+    print  request.values
     if request.values.get("mid",None) and request.values.get("update_time",None):
         result=dark_status.query.filter_by(mid= request.values["mid"]).all()
         if len(result)==0:
-            if request.values["new_config_version"] == request.values["old_config_version"]:
+            if request.values["new_config_version"] == request.values["old_config_version"] and request.values["boot_time"] != "":
                 status=1
             else:
                 status=0
             try:
-                db.session.add(dark_status(mid=request.values["mid"],dark_version=request.values["dark_version"],new_config_version=request.values["new_config_version"],old_config_version=request.values["old_config_version"],status=status,boot_time=request.values["boot_time"],update_time=request.values["update_time"]))
+                if request.values["boot_time"]=="":
+                    db.session.add(dark_status(mid=request.values["mid"],dark_version=request.values["dark_version"],new_config_version=request.values["new_config_version"].split("-")[-1],old_config_version=request.values["old_config_version"].split("-")[-1],status=status,update_time=request.values["update_time"],dark_num=request.values["dark_num"]))
+                else:
+                    db.session.add(dark_status(mid=request.values["mid"],dark_version=request.values["dark_version"],new_config_version=request.values["new_config_version"].split("-")[-1],old_config_version=request.values["old_config_version"].split("-")[-1],status=status,boot_time=request.values["boot_time"],update_time=request.values["update_time"],dark_num=request.values["dark_num"]))
                 db.session.commit()
             except Exception,e:
                 print e
@@ -41,16 +58,19 @@ def collect_info():
                 return jsonify({"error":0,"message":"status upload succeed."}),201
         else:
             query_result=dark_status.query.filter_by(mid=request.values["mid"]).first()
-            if request.values["new_config_version"] == request.values["old_config_version"]:
+            if request.values["new_config_version"] == request.values["old_config_version"] and request.values["boot_time"] != "":
                 status=1
             else:
                 status=0
             query_result.status=status
             query_result.dark_version=request.values["dark_version"]
-            query_result.new_config_version=request.values["new_config_version"]
-            query_result.old_config_version=request.values["old_config_version"]
+            query_result.new_config_version=request.values["new_config_version"].split("-")[-1]
+            query_result.old_config_version=request.values["old_config_version"].split("-")[-1]
             query_result.update_time=request.values["update_time"]
-            query_result.boot_time=request.values["boot_time"]
+            if request.values["boot_time"]=="":
+                pass
+            else:
+                query_result.boot_time=request.values["boot_time"]
             db.session.commit()
             return jsonify({"error":0,"message":"status update succeed."}),201
     else:
--- model.py
+++ model.py
@@ -1,7 +1,16 @@
 from flask import Flask
 from flask_script import Manager
 from flask_sqlalchemy import SQLAlchemy
+from datetime import date, datetime
 import json
+class MyEncoder(json.JSONEncoder):  
+  def default(self, obj):  
+      if isinstance(obj, datetime):  
+          return obj.strftime('%Y-%m-%d %H:%M:%S')  
+      elif isinstance(obj, date):  
+          return obj.strftime('%Y-%m-%d')  
+      else:  
+          return json.JSONEncoder.default(self, obj)
 app=Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI']='mysql://root:chinatt()^^*@localhost:3306/darkinfo'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=True
@@ -15,10 +24,10 @@ class dark_status(db.Model):
     new_config_version = db.Column(db.String(320))
     old_config_version = db.Column(db.String(320))
     status = db.Column(db.Boolean, nullable=False)
-    boot_time = db.Column(db.DateTime, nullable=False)
+    dark_num = db.Column(db.Integer)
+    boot_time = db.Column(db.DateTime, nullable=True)
     update_time = db.Column(db.DateTime, nullable=False)
     def __repr__(self):
-#        return '<dark_mid %r>' % self.mid
-        return json.dumps({"mid":self.mid,"dark_version":self.dark_version,"id":self.id,"new_config_version":self.new_config_version,"old_config_version":self.old_config_version,"status":self.old_config_version,"boot_time":self.boot_time,"update_time":self.update_time}) 
+        return json.dumps({"mid":self.mid,"dark_version":self.dark_version,"id":self.id,"new_config_version":self.new_config_version,"old_config_version":self.old_config_version,"status":self.old_config_version,"boot_time":self.boot_time,"update_time":self.update_time,"dark_num":self.dark_num},cls=MyEncoder) 
 if __name__=="__main__":
     manager.run()
